5c8e1585f10dc513419db91ceac50ec2f87c4b67
Vladyslav Yakubenko <vladikyakubenko@gmail.com>
2023-07-06 13:21:32 +0300

    Added self-destruct platform after the distance traveled and reappear at the initial distance

--- Assets/InfinityRunner/Scripts/Platform/PlatformGenerator.cs
+++ Assets/InfinityRunner/Scripts/Platform/PlatformGenerator.cs
@@ -1,39 +1,69 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _platformPrefabs;
-    [SerializeField] private float _spawnInterval;
+    [SerializeField] private GameObject _platformPrefab;
+    [SerializeField] private float _spawnDistance;
+    [SerializeField] private int _maxPlatforms;
     [SerializeField] private float _platformSpeed;
+    [SerializeField] private float _spawnInterval;
 
-    private float timer = 0f;
+    private List<GameObject> spawnedPlatforms = new List<GameObject>();
+    private float lastSpawnedPosition;
+
+    private void Start()
+    {
+        lastSpawnedPosition = transform.position.z;
+        StartCoroutine(SpawnPlatformCoroutine());
+    }
 
     private void Update()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= _spawnInterval)
+        if (transform.position.z - lastSpawnedPosition >= _spawnDistance)
         {
             SpawnPlatform();
-            timer = 0f;
+        }
+
+        if (spawnedPlatforms.Count > _maxPlatforms)
+        {
+            Destroy(spawnedPlatforms[0]);
+            spawnedPlatforms.RemoveAt(0);
         }
     }
 
     private void SpawnPlatform()
     {
-        int randomIndex = Random.Range(0, _platformPrefabs.Length);
-        GameObject newPlatform = Instantiate(_platformPrefabs[randomIndex], transform.position, Quaternion.identity);
+        GameObject newPlatform = Instantiate(_platformPrefab, transform.position, Quaternion.identity);
+        spawnedPlatforms.Add(newPlatform);
+        lastSpawnedPosition = transform.position.z;
+
         StartCoroutine(MovePlatform(newPlatform));
+    }
+
+    private IEnumerator SpawnPlatformCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_spawnInterval);
+            SpawnPlatform();
+        }
     }
 
     private IEnumerator MovePlatform(GameObject platform)
     {
-        while (true)
+        Vector3 initialPosition = platform.transform.position;
+        float distanceTraveled = 0f;
+
+        while (distanceTraveled < _spawnDistance)
         {
             platform.transform.Translate(Vector3.back * _platformSpeed * Time.deltaTime);
+            distanceTraveled = Mathf.Abs(platform.transform.position.z - initialPosition.z);
             yield return null;
         }
+
+        platform.transform.position = initialPosition;
+        StartCoroutine(MovePlatform(platform));
     }
 }
-
